e7213c076dc67d4d9cca063da02c54efe3aeb4aa
ZHENBAO YANG <315672028@qq.com>
2013-09-15 17:43:27 -0400

    1
     1

---
+++ Triangle_Test/src/yzb/Triangle.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+/**
+ * @author Yzb
+ *
+ */
+package yzb;
+import java.lang.Math;
+import java.math.BigDecimal;
+public class Triangle
+{
+	protected double A=0;
+	protected double B=0;
+	protected double C=0;
+	
+	public Triangle(double A,double d,double C)
+	{
+		this.A=A;
+		this.B=d;
+		this.C=C;
+	}
+	
+	public boolean isTriangle(Triangle triangle)
+	{
+		boolean isTrue=false;
+		if((triangle.A<(triangle.B+triangle.C))
+				&&(triangle.B<(triangle.A+triangle.C))
+				&&(triangle.C<(triangle.A+triangle.B)))
+			isTrue=true;
+		return isTrue;
+	}
+	
+	public String isType(Triangle triangle)
+	{
+		String strType="not a triangle.";
+		if(this.isTriangle(triangle))
+		{
+			if(triangle.A==triangle.B&&triangle.B==triangle.C)
+				strType="equilateral.";
+			else if((triangle.A!=triangle.B)&&
+					(triangle.B!=triangle.C)&&
+					(triangle.A!=triangle.C))
+			{
+				if((triangle.A*triangle.A==triangle.B*triangle.B+triangle.C*triangle.C)
+				  ||(triangle.B*triangle.B==triangle.A*triangle.A+triangle.C*triangle.C)
+				  ||(triangle.C*triangle.C==triangle.A*triangle.A+triangle.B*triangle.B))
+					strType="scalene, and it is a right triangle as well.";
+				else
+					strType="scalene, and it is not a right triangle as well.";
+			}
+			else
+			{
+				if((triangle.A*triangle.A==triangle.B*triangle.B+triangle.C*triangle.C)
+				  ||(triangle.B*triangle.B==triangle.A*triangle.A+triangle.C*triangle.C)
+				  ||(triangle.C*triangle.C==triangle.A*triangle.A+triangle.B*triangle.B))
+					strType="isosceles, and it is a right triangle as well.";
+				else
+					strType="isosceles, and it is not a right triangle as well.";
+			}
+		}
+		return strType;
+    }
+	
+	public static void main(String args[])
+	{
+		//BigDecimal a = new BigDecimal(1000);
+		//return a.doubleValue();
+		Triangle tri = new Triangle(3,4,5);
+		//if ( tri.isTriangle(tri) )
+		System.out.println("This is "+tri.isType(tri));
+		//else 
+			//System.out.println("This is not triangle.");	
+	}
+
+}
+    
---
+++ Triangle_Test/src/yzb/TriangleTest.java
@@ -0,0 +1,94 @@
+package yzb;
+
+import junit.framework.*;
+public class TriangleTest extends TestCase {
+    public Triangle triangle = null; 
+    public void testIsTriangle01() {
+    	triangle = new Triangle(0, 3, 3);
+        boolean expected = false;
+        boolean result = triangle.isTriangle(triangle);
+        boolean actual = result;
+        assertEquals(expected, actual);
+     }
+    public void testType01() {
+        triangle = new Triangle(0, 3, 3);
+        String expected = "not a triangle.";																
+        String result = triangle.isType(triangle);
+        String actual = result;
+        assertEquals(expected, actual);
+    }  
+    public void testIsTriangle02() {
+    	triangle = new Triangle(3, 3, 3);
+        boolean expected = true;
+        boolean result = triangle.isTriangle(triangle);
+        boolean actual = result;
+        assertEquals(expected, actual);
+     }
+    public void testType02() {
+        triangle = new Triangle(3, 3, 3);
+        String expected = "equilateral.";																
+        String result = triangle.isType(triangle);
+        String actual = result;
+        assertEquals(expected, actual);
+    }   
+    public void testIsTriangle03() {
+    	triangle = new Triangle(3, 4, 5);
+        boolean expected = true;
+        boolean result = triangle.isTriangle(triangle);
+        boolean actual = result;
+        assertEquals(expected, actual);
+     }
+    public void testType03() {
+        triangle = new Triangle(3, 4, 5);
+        String expected = "scalene, and it is a right triangle as well.";																
+        String result = triangle.isType(triangle);
+        String actual = result;
+        assertEquals(expected, actual);
+    }  
+    public void testIsTriangle04() {
+    	triangle = new Triangle(3, 4, 6);
+        boolean expected = true;
+        boolean result = triangle.isTriangle(triangle);
+        boolean actual = result;
+        assertEquals(expected, actual);
+     }
+    public void testType04() {
+        triangle = new Triangle(3, 4, 6);
+        String expected = "scalene, and it is not a right triangle as well.";																
+        String result = triangle.isType(triangle);
+        String actual = result;
+        assertEquals(expected, actual);
+    }   
+    public void testIsTriangle05() {
+    	triangle = new Triangle(1, Math.sqrt(2), 1);
+        boolean expected = true;
+        boolean result = triangle.isTriangle(triangle);
+        boolean actual = result;
+        assertEquals(expected, actual);
+     }
+    public void testType05() {
+    	triangle = new Triangle( 1, Math.sqrt(2),  1);
+        String expected = "isosceles, and it is a right triangle as well.";																
+        String result = triangle.isType(triangle);
+        String actual = result;
+        assertEquals(expected, actual);
+    }
+    public void testIsTriangle06() {
+    	triangle = new Triangle(3, 4, 3);
+        boolean expected = true;
+        boolean result = triangle.isTriangle(triangle);
+        boolean actual = result;
+        assertEquals(expected, actual);
+     }
+    public void testType06() {
+        triangle = new Triangle(3, 4, 3);
+        String expected = "isosceles, and it is not a right triangle as well.";																
+        String result = triangle.isType(triangle);
+        String actual = result;
+        assertEquals(expected, actual);
+    }
+}
+
+
+
+
